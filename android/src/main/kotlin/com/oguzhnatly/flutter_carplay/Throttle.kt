ed0f61c47e5d1f6391c46e0c669898eda37f273d
Imran Vora <124134058+ImranVoraAubi@users.noreply.github.com>
2024-07-24 18:21:46 +0530

    update map surface flow to resolve redirection to map template issue

--- android/src/main/kotlin/com/oguzhnatly/flutter_carplay/Throttle.kt
+++ android/src/main/kotlin/com/oguzhnatly/flutter_carplay/Throttle.kt
@@ -9,31 +9,30 @@ import kotlinx.coroutines.launch
 /**
  *  A utility class for debouncing high-frequency events.
  *
- *  Debouncing ensures that high-frequency events (like rapid button clicks) trigger functionality
+ *  Throttling ensures that high-frequency events (like rapid button clicks) trigger functionality
  *  such as network requests or animations only once in a specified time interval, instead of for
  *  each and every event.
  *
  *  This is accomplished by delaying the action (specified as a lambda function) by a fixed
- *  interval. If a new event occurs within this interval, the previous event and its associated
- *  action get cancelled and a new delay period starts for the latest event.
+ *  interval. If a new event occurs within this interval, the current event is completed then only
+ *  new event is created otherwise it will return immediately.
  *
- *  @property scope The [CoroutineScope] in which the debounce operations are launched.
+ *  @property scope The [CoroutineScope] in which the throttle operations are launched.
  */
 class Throttle(private val scope: CoroutineScope) {
-    /// A job represents a cancellable piece of work. In this case, the work is the debounced
-    /// action that will be executed after a certain delay.
-    /// The job is cancelled if a new event comes in before the delay period ends.
+
     private var job: Job? = null
 
 
     /**
      * Delays the execution of the specified [action] by the [interval].
      *
-     * If this method is called again before the [interval] is over, the previous [action] is
-     * cancelled and the delay resets for the latest invocation of the method.
+     * If this method is called again before the [interval] is over, If a new event occurs within
+     * this interval, the current event is completed then only new event is created otherwise
+     * it will return immediately.
      *
      * @param interval The time in milliseconds to wait before executing the [action].
-     * @param action The action to execute after the delay.
+     * @param action The action to execute.
      */
     fun throttle(interval: Long, action: () -> Unit) {
         if(job?.isCompleted == false) return // Cancel the previous job if it hasn't completed yet.
